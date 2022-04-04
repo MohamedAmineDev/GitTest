40437fff29a86bc6740349f32bf4fe8bbc218fdb
MohamedAmineDev <mohamedaminebensafta@gmail.com>
2022-04-04 21:53:07 +0100

    My first java project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 1000) {
+            System.out.println("I'm intellij idea the text editor");
+            i++;
+        }
+    }
+}
